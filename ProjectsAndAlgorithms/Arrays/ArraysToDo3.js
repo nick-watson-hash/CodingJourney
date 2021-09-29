25b132cbf522a4d8e03b969facd5ff051b534003
NickW <nicholas.watson@mailfence.com>
2021-09-28 22:58:05 -0700

    Completed #1

--- ProjectsAndAlgorithms/Arrays/ArraysToDo3.js
+++ ProjectsAndAlgorithms/Arrays/ArraysToDo3.js
@@ -1,11 +1,22 @@
 // #1
-//#take out remove function
 function removeNegatives(arr) {
-    for (var x = 0; x < arr.length; x++ ) {
-        if (arr[x] < 0) {
-            arr[x].remove
+    for (var x = 0, i = 0, z = arr.length; x < z; x++  ) {
+        if (arr[x] >= 0) {
+            arr[i++] = arr[x]
+        }
+    }
+    arr.length = i
+    return arr
+}
+console.log(removeNegatives([56,22,-3,-36,-90,34,12]))
+
+// #2 
+function secondToLast(arr) {
+    for (var x = 0; x < arr.length; x++  ) {
+        if (arr[x] != String ) {
+            arr[x++] = arr[x]
         }
     }
     return arr
 }
-console.log(removeNegatives([56,56,-3,-3,-90,34,12]))
+console.log(secondToLast([42,true,4,"Kate",7]))
